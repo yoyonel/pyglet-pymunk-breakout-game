7c34c6817db73e118b48954e918c7e5683d6e820
ATTY Lionel <yoyonel@hotmail.com>
2018-05-07 13:42:57 +0200

    Bricks: parametrize version for building a grid of bricks. W.I.P.

--- src/pyglet_pymunk/breakout_game/components/bricks.py
+++ src/pyglet_pymunk/breakout_game/components/bricks.py
@@ -2,6 +2,7 @@
 
 """
 import pymunk
+from pymunk import Vec2d
 
 
 class Bricks:
@@ -11,6 +12,10 @@ class Bricks:
         :param space:
         :param collision_type_for_brick:
         """
+        # self.build_grid(space, collision_type_for_brick, collision_type_for_ball, aspect_ratio)
+        self.build_dynamic_grid(space, collision_type_for_brick, collision_type_for_ball, aspect_ratio)
+
+    def build_grid(self, space: pymunk.Space, collision_type_for_brick, collision_type_for_ball, aspect_ratio):
         grid_position = aspect_ratio.scale(90, 500)
 
         brick_size = aspect_ratio.scale(100, 16)
@@ -25,9 +30,9 @@ class Bricks:
         brick_step_y = brick_height + y_space_between_brick
 
         for x in range(10):
-            pos_x = grid_position.x + x*brick_step_x
+            pos_x = grid_position.x + x * brick_step_x
             for y in range(10):
-                pos_y = grid_position.y + y*brick_step_y
+                pos_y = grid_position.y + y * brick_step_y
                 # http://www.pymunk.org/en/latest/pymunk.html#pymunk.Body.__init__
                 body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
                 # position
@@ -38,6 +43,46 @@ class Bricks:
                     (0, 0),
                     aspect_ratio.scale(100, 0),
                     8
+                )
+                shape.elasticity = 0.98
+                shape.collision_type = collision_type_for_brick
+                space.add(body, shape)
+
+        handler = space.add_collision_handler(collision_type_for_brick, collision_type_for_ball)
+        # http://www.pymunk.org/en/latest/pymunk.html#pymunk.CollisionHandler.separate
+        handler.separate = self._cb_remove_brick
+
+    def build_dynamic_grid(self, space: pymunk.Space, collision_type_for_brick, collision_type_for_ball, aspect_ratio):
+        wall_left = 50
+        wall_right = 1230
+
+        grid_left_corner = aspect_ratio.scale(wall_left + 40, 500)
+        grid_right_corner = aspect_ratio.scale(wall_right - 40, 500 + (16+14)*10)
+
+        nb_bricks = Vec2d(8, 4)
+
+        spaces_between_brick = aspect_ratio.scale_V2d(Vec2d(10, 14))
+
+        grid_size = grid_right_corner - grid_left_corner
+
+        brick_size = (grid_size - (nb_bricks - Vec2d(1, 1)) * spaces_between_brick) / nb_bricks
+
+        brick_steps = brick_size + spaces_between_brick
+
+        for x in range(nb_bricks.x):
+            pos_x = grid_left_corner.x + x * brick_steps.x
+            for y in range(nb_bricks.y):
+                pos_y = grid_left_corner.y + y * brick_steps.y
+                # http://www.pymunk.org/en/latest/pymunk.html#pymunk.Body.__init__
+                body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+                # position
+                body.position = pos_x, pos_y
+                # shape
+                shape = pymunk.Segment(
+                    body,
+                    (0, 0),
+                    Vec2d(brick_size.x, 0),
+                    brick_size.y / 2.0,
                 )
                 shape.elasticity = 0.98
                 shape.collision_type = collision_type_for_brick
